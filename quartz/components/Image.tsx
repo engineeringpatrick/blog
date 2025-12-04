69aabf2581439927bff62b9b8859cd48d7eb530e
Patrick Deniso <patrickdeniso@gmail.com>
2025-12-04 07:21:14 -0500

    fix sidebar image taking up too much space on smaller screens

--- quartz/components/Image.tsx
+++ quartz/components/Image.tsx
@@ -4,14 +4,27 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 const Image: QuartzComponent = ({displayClass}: QuartzComponentProps) => {
   return (
       <a 
+        class={classNames(displayClass, "sidebar-a")}
         href={`https://www.weforum.org/stories/2021/12/humans-multiplanetary-species`}
         target={`_blank`}
         >
         <img 
-            class={classNames(displayClass, "image")} 
+            class={classNames(displayClass, "sidebar-img")} 
             src={`/static/astronaut.jpeg`}
         />
     </a>
   )
 }
+
+Image.css = `
+.sidebar-a {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+.sidebar-img {
+  opacity: 0.5;
+  object-fit: cover;
+}
+`
 export default (() => Image) satisfies QuartzComponentConstructor;
